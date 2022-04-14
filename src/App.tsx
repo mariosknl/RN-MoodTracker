8d0bec29e788d933443b13e692848d4df46eec8f
mariosknl <mariosknl@outlook.com>
2022-04-14 18:08:54 +0300

    adding app icons and splash screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { AppProvider } from './App.provider';
 import { BottomTabsNavigator } from './screens/BottomTabs.navigator';
 
 export const App: React.FC = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <AppProvider>
       <NavigationContainer>
