f7ac21b1ca6e668ed2207de66c9413737a1e624d
mariosknl <mariosknl@outlook.com>
2022-04-14 14:17:12 +0300

    adding vector icons

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -1,5 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { Text } from 'react-native';
+import { HomeIcon } from '../components/Icons';
 import { Analytics } from './Analytics.screen';
 import { History } from './History.screen';
 import { Home } from './Home.screen';
@@ -8,7 +10,15 @@ const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({}) => {
+          if (route.name === 'Home') {
+            return <HomeIcon />;
+          }
+          return <Text>{route.name}</Text>;
+        },
+      })}>
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="History" component={History} />
       <BottomTabs.Screen name="Analytics" component={Analytics} />
